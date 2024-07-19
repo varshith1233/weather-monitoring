7bfed426ed9bcb54509d47d9863f34a46ed239ca
varshith1233 <varshithpyla@gmail.com>
2024-07-19 18:13:21 +0530

    first commit

---
+++ consumer.py
@@ -0,0 +1,89 @@
+from confluent_kafka import Consumer, KafkaException
+import pymongo
+import json
+import urllib.parse
+
+    # Function to handle delivery reports
+def delivery_report(err, msg):
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+    # Function to check MongoDB connection
+def check_mongodb_connection(mongo_uri):
+    try:
+        client = pymongo.MongoClient(mongo_uri)
+        db = client.admin
+        server_info = db.command('serverStatus')
+        print("MongoDB connection successful.")
+        return True
+    except Exception as e:
+        print(f"MongoDB connection error: {e}")
+        return False
+
+    # Function to consume weather data and save to MongoDB
+def consume_and_save_to_mongodb(consumer, topic, mongo_uri):
+    try:
+        client = pymongo.MongoClient(mongo_uri)
+        db = client["kafkaproj"]  # Your Database name
+        collection = db["weather_collection"]  # Your mongo collection name
+
+        consumer.subscribe([topic])
+
+        while True:
+            msg = consumer.poll(1.0)
+
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaException._PARTITION_EOF:
+                        # End of partition event, not an error.
+                    continue
+                else:
+                    print(msg.error())
+                    break
+
+            try:
+                    # Decode the message value from Kafka and convert it to a Python dictionary
+                weather_data = json.loads(msg.value().decode('utf-8'))
+                print(weather_data)
+
+                    # Drop the 'weather_icons' field if it exists
+                if 'current' in weather_data and 'weather_icons' in weather_data['current']:
+                    del weather_data['current']['weather_icons']
+
+                    # Insert the modified data into MongoDB
+                collection.insert_one(weather_data)
+
+                print('Received message and saved to MongoDB:', weather_data)
+            except (json.JSONDecodeError, TypeError) as e:
+                print(f"Error decoding JSON: {e}")
+                print('Received raw message:', msg.value())
+
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"Error in consumer: {e}")
+    finally:
+        consumer.close()
+
+    # MongoDB connection setup
+    #username = urllib.parse.quote_plus("pintu")
+    #password = urllib.parse.quote_plus("MarchiPoya@12")
+mongo_uri = f"mongodb+srv://sayaniaram:sayaniaram@spark.65sppsm.mongodb.net/?retryWrites=true&w=majority&appName=spark"
+
+    # Check MongoDB connection
+if check_mongodb_connection(mongo_uri):
+        # Kafka consumer configuration
+    consumer_conf = {'bootstrap.servers': 'localhost:9092', 'group.id': 'weather-consumer', 'auto.offset.reset': 'earliest'}
+    consumer = Consumer(consumer_conf)
+
+        # Kafka topic to consume from
+    weather_topic = 'real_weather'
+
+        # Consume and save to MongoDB
+    consume_and_save_to_mongodb(consumer, weather_topic, mongo_uri)
+else:
+    print("MongoDB connection failed. Cannot proceed with data consumption.")
+
---
+++ producer.py
@@ -0,0 +1,37 @@
+import requests
+from confluent_kafka import Producer
+import json
+
+def get_current_weather_data(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {'q': city, 'appid': api_key}
+    response = requests.get(base_url, params=params)
+    return response.json() if response.status_code == 200 else None
+
+def delivery_report(err, msg):
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+def produce_current_weather_data(api_key, cities, producer, topic):
+    for city in cities:
+        weather_data = get_current_weather_data(api_key, city)
+        if weather_data:
+            try:
+                json_data = json.dumps(weather_data)
+                
+                producer.produce(topic,key=city, value=json_data, callback=delivery_report)
+                producer.flush()
+            except Exception as e:
+                print(f"Error producing message: {e}")
+
+producer_conf = {'bootstrap.servers': 'localhost:9092'}
+producer = Producer(producer_conf)
+weather_topic = 'real_weather'
+api_key ='5578e1cea9e0cc8e85cb6bc123343faa'
+
+with open(r"cities.txt", 'r') as file:
+    cities = [line.strip() for line in file]
+
+produce_current_weather_data(api_key, cities, producer, weather_topic)
